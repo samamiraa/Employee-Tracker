686e1e94bb6fdfed35905e15649078b736dabcef
Samantha Allen <samanthaamiraallen@gmail.com>
2024-01-26 15:39:30 -0400

    added create table for role

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,12 @@ CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
 );
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    FOREIGN KEY (department_id),
+    REFERENCES department(id),
+    ON DELETE SET NULL
+);
