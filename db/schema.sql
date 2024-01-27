e25682b3606bffb2e5a5503f2ce0eaa197747cf3
Samantha Allen <samanthaamiraallen@gmail.com>
2024-01-27 08:46:19 -0400

    edited create table in schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,24 +4,25 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    departmentId INT NOT NULL,
     name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(departmentId)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    roleId INT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    FOREIGN KEY (department_id),
-    REFERENCES department(id),
-    ON DELETE SET NULL
+    PRIMARY KEY(roleId),
+    FOREIGN KEY department(departmentId)
+    REFERENCES department(id)
 );
 
 CREATE TABLE  employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    employeeId INT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    FOREIGN KEY (role_id),
-    REFERENCES role(id),
-    ON DELETE SET NULL
+    PRIMARY KEY(employeeId),
+    FOREIGN KEY role(roleId)
+    REFERENCES role(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,8 @@
 INSERT INTO department (id, name)
 VALUES 
+
+INSERT INTO role (id, title, salary)
+VALUES
+
+INSERT INTO employee (id, first_name, last_name)
+VALUES
