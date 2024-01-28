47a065ac7a70326f0325f3c37f0ed70a9f30423d
Samantha Allen <samanthaamiraallen@gmail.com>
2024-01-28 11:07:53 -0400

    added seeds for department and role

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,8 +5,18 @@ VALUES  (1, "Sales"),
         (4, "Bakery"),
         (5, "Meat"),
 
-INSERT INTO role (id, title, salary)
-VALUES  (1, "Cashier", )
+INSERT INTO role (id, title, salary, department_id)
+VALUES  (1, "Cashier", 30000.00, 1),
+        (2, "Sales Manager", 50000.00, 1),
+        (3, "Deli Food Service Assistant", 30000.00, 2),
+        (4, "Meat Cutter", 40000.00, 2),
+        (5, "Deli Manager", 50000.00, 2),
+        (6, "Produce Stocker", 30000.00, 3),
+        (7, "Produce Manager", 50000.00, 3),
+        (8, "Baker", 40000.00, 4),
+        (9, "Bakery Manager", 50000.00, 4),
+        (10, "Butcher", 40000.00, 5),
+        (11, "Meat Departmente Manager", 50000.00, 5)
 
-INSERT INTO employee (id, first_name, last_name)
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES
